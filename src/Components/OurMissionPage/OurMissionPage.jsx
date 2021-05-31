c5909214050a86203ed675c20610a1899c0db504
Joshua Kern <joshuakerndev@gmail.com>
2021-05-30 23:46:27 -0700

    Work on parallax effect for our-mission page

--- src/Components/OurMissionPage/OurMissionPage.jsx
+++ src/Components/OurMissionPage/OurMissionPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './OurMissionPage.scss';
+import missionImage1 from '../../assets/imgs/16.jpg'
 
 const OurMissionPage = () => {
 
@@ -25,10 +26,24 @@ const OurMissionPage = () => {
                 className="OurMissionDiv"
                 style={{ backgroundPositionY: `${scrollTop*0.7}px` }}
             >
-                <h1 className="OurMissionHeader">Div1</h1>
+                <h1 className="OurMissionHeader">What is the Secret to Cultivate?</h1>
             </div>
             <div className="OurMissionDiv">
-                <h1 className="OurMissionHeader">Div2</h1>
+                <div className="OurMissionText1">
+                    <h1>About Us</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget 
+                        ultrices diam. Sed posuere ultrices congue. Morbi laoreet rhoncus 
+                        porta. Vestibulum eleifend efficitur imperdiet. Ut feugiat volutpat 
+                        lectus, vestibulum elementum nisl. Suspendisse potenti. Vivamus mollis 
+                        sit amet arcu at bibendum.
+                    </p>
+                </div>
+                <div className="OurMissionImage1">
+                    <img
+                        src={missionImage1}
+                        alt="Planter seedlings" />
+                </div>
             </div>
             <div className="OurMissionDiv">
                 <h1 className="OurMissionHeader">Div3</h1>
